62b3d2dbc3cf4e69613948af30acd8d5b80cec6b
encodedBicoding <dominicisioma000@gmail.com>
2019-09-19 18:29:26 +0100

    implements lift buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Button from './components/Button';
+import liftData from './data';
 import './App.scss';
 
 const App = () => {
 
   return (
-    <div>
-      <div>
-        <div>
-          <Button value="hey" />
+    <div className="flex">
+      <div className="btn-space">
+        <div className="btn-holder">
+          {
+            liftData.map((lift, idx) => (
+              <Button key={idx} name={lift.name} value={lift.name} />
+            ))
+          }
         </div>
       </div>
     </div>
