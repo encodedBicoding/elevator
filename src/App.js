d9918005cd78a63a46ef4d243a3ece0122e983b4
encodedBicoding <dominicisioma000@gmail.com>
2019-09-19 23:32:20 +0100

    feat(lift):implement lift system

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import Button from './components/Button';
+import Container from './components/Container';
 import liftData from './data';
+import Lift from './components/Lift';
 import './App.scss';
 
 const App = () => {
 
   return (
     <div className="flex">
-      <div className="btn-space">
+      <div className="main-space">
         <div className="btn-holder">
           {
             liftData.map((lift, idx) => (
               <Button key={idx} name={lift.name} value={lift.name} />
             ))
           }
+        </div>
+        <div className="ctn-holder">
+          {
+            liftData.map((container, idx) => (
+              <Container key={idx} />
+            ))
+          }
+          <Lift />
         </div>
       </div>
     </div>
